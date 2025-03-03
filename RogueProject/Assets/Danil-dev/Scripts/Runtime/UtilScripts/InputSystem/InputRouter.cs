9764dc8dda37ef5fb59ecfbf56533d6fca52b955
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2025-03-04 01:01:29 +0300

    .

---
+++ RogueProject/Assets/Danil-dev/Scripts/Runtime/UtilScripts/InputSystem/InputRouter.cs
@@ -0,0 +1,115 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static InputActions;
+
+namespace Danil_dev.Scripts.Runtime.UtilScripts.InputSystem
+{
+    [CreateAssetMenu(menuName = "D-Dev/InputRouter")]
+    public class InputRouter : ScriptableObject, IPlayerActions
+    {
+        #region Fields
+
+        private InputActions _inputActions;
+        
+        public event Action<Vector2> Move;
+        public event Action<Vector2> Look;
+        public event Action<bool> JumpPressed;
+        public event Action<bool> EscPressed;
+        public event Action<bool> InteractPressed;
+        public event Action<bool> RmbPressed;
+        public event Action<bool> LmbPressed;
+
+        #endregion
+
+        #region IInputRouter
+
+        public void Enable()
+        {
+            if (_inputActions == null)
+            {
+                _inputActions = new InputActions();
+                _inputActions.Player.SetCallbacks(this);
+            }
+            _inputActions.Enable();
+        }
+
+        public void Disable() => _inputActions?.Disable();
+
+        #endregion
+
+        #region IPlayerActions
+
+        public void OnLook(InputAction.CallbackContext context) => Look?.Invoke(context.ReadValue<Vector2>());
+
+        public void OnMove(InputAction.CallbackContext context) => Move?.Invoke(context.ReadValue<Vector2>());
+
+        public void OnLmb(InputAction.CallbackContext context)
+        {
+            switch (context.phase)
+            {
+                case InputActionPhase.Started:
+                    LmbPressed?.Invoke(true);
+                    break;
+                case InputActionPhase.Canceled:
+                    LmbPressed?.Invoke(false);
+                    break;
+            }
+        }
+
+        public void OnRmb(InputAction.CallbackContext context)
+        {
+            switch (context.phase)
+            {
+                case InputActionPhase.Started:
+                    RmbPressed?.Invoke(true);
+                    break;
+                case InputActionPhase.Canceled:
+                    RmbPressed?.Invoke(false);
+                    break;
+            }
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            switch (context.phase)
+            {
+                case InputActionPhase.Started:
+                    InteractPressed?.Invoke(true);
+                    break;
+                case InputActionPhase.Canceled:
+                    InteractPressed?.Invoke(false);
+                    break;
+            }
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            switch (context.phase)
+            {
+                case InputActionPhase.Started:
+                    JumpPressed?.Invoke(true);
+                    break;
+                case InputActionPhase.Canceled:
+                    JumpPressed?.Invoke(false);
+                    break;
+            }
+        }
+
+        public void OnEsc(InputAction.CallbackContext context)
+        {
+            switch (context.phase)
+            {
+                case InputActionPhase.Started:
+                    EscPressed?.Invoke(true);
+                    break;
+                case InputActionPhase.Canceled:
+                    EscPressed?.Invoke(false);
+                    break;
+            }
+        }
+
+        #endregion
+      
+    }
+}
